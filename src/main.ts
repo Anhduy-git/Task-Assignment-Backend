409b3eb3ccb1af572a1c53b9aaa8dcaa523ba4d3
Nguyễn Trần Anh Duy <84486806+Anhduy-git@users.noreply.github.com>
2022-11-01 20:32:24 +0700

    update configs

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new ConfigService();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, //only use field list in dto
       transform: true,
     }),
   );
-  await app.listen(3008);
+  await app.listen(config.get('PORT'));
 }
 bootstrap();
